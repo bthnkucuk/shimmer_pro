d3f063492e5d15c603faa4b762b7b78ed8bccfcf
bthnkucuk <84974284+bthnkucuk@users.noreply.github.com>
2022-11-13 17:43:32 +0300

    //

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
+# Shimmer Pro
 
+
+Shimmer effect with easy usage.
+
+<p>
+
+</p>
+
+## How to use
+
+```dart
+import 'package:shimmer_pro/shimmer_pro.dart';
+```
+
+```dart
+    ShimmerPro.text(
+              light: shimmerlight,
+              width: 450,
+              scaffoldBackgroundColor: bgColor,
+            ),
+```
+
+```dart
+  ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                      borderRadius: 50,
+                    ),
+```
+
+```dart
+   ShimmerPro.generated(
+                light: shimmerlight,
+                scaffoldBackgroundColor: bgColor,
+                child: Row(
+                  children: [
+                    ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                    ),
+                    ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                    ),
+                    ShimmerPro.text(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      width: 150,
+                    )
+                  ],
+                )),
+```
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,39 +8,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shimmer Pro',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Shimmer Pro'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -49,69 +30,124 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool isThemeDark = true;
+  late Color bgColor;
+  late ShimmerProLight shimmerlight;
+  @override
+  void initState() {
+    super.initState();
+    _themeMode();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _themeMode() {
+    isThemeDark = !isThemeDark;
+    if (isThemeDark) {
+      setState(() {
+        bgColor = const Color.fromARGB(255, 50, 50, 50);
+        shimmerlight = ShimmerProLight.lighter;
+      });
+    } else {
+      setState(() {
+        bgColor = const Color.fromARGB(255, 240, 240, 240);
+        shimmerlight = ShimmerProLight.darker;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: bgColor,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            ShimmerPro.sized(width: 100, height: 100),
-            const Text(
-              'You have pushed the button this many times:',
+            const SizedBox(height: 20),
+            ShimmerPro.text(
+              light: shimmerlight,
+              width: 450,
+              scaffoldBackgroundColor: bgColor,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ShimmerPro.generated(
+                light: shimmerlight,
+                scaffoldBackgroundColor: bgColor,
+                child: Row(
+                  children: [
+                    ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                      borderRadius: 50,
+                    ),
+                    ShimmerPro.text(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      width: 270,
+                    )
+                  ],
+                )),
+            ShimmerPro.generated(
+                light: shimmerlight,
+                scaffoldBackgroundColor: bgColor,
+                child: Row(
+                  children: [
+                    ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                    ),
+                    ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                    ),
+                    ShimmerPro.text(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      width: 150,
+                    )
+                  ],
+                )),
+            ShimmerPro.generated(
+                light: shimmerlight,
+                width: 200,
+                scaffoldBackgroundColor: bgColor,
+                child: Column(
+                  children: [
+                    ShimmerPro.sized(
+                      light: shimmerlight,
+                      scaffoldBackgroundColor: bgColor,
+                      height: 100,
+                      width: 100,
+                      borderRadius: 50,
+                    ),
+                    ShimmerPro.text(
+                        light: shimmerlight,
+                        maxLine: 1,
+                        scaffoldBackgroundColor: bgColor,
+                        width: 200)
+                  ],
+                )),
+            ShimmerPro.text(
+              light: shimmerlight,
+              maxLine: 5,
+              width: 450,
+              scaffoldBackgroundColor: bgColor,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _themeMode,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
